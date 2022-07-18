de3fa566a01c77bb82a4ba88bd90906945d9c40d
sumitra05 <sumitra9039sumi@gmail.com>
2022-07-18 20:25:39 +0530

    backend

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+	try {
+		mongoose.connect("mongodb+srv://sumitra05:Abc123@cluster0.uj2icdv.mongodb.net/userDetail?retryWrites=true&w=majority", connectionParams);
+		console.log("Connected to database successfully");
+	} catch (error) {
+		console.log(error);
+		console.log("Could not connect database!");
+	}
+};
